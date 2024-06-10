92451ec292712b0cd20e21840a44399d0169c629
Mashapa <mashapakarabo9@gmail.com>
2024-06-10 16:23:29 +0200

    first commit

---
+++ Data_types.js
@@ -0,0 +1,5 @@
+//DataType Operators
+//Execise 1 : Numbers
+
+num_int = 43;
+num_float = 32.53;
---
+++ variable.js
@@ -0,0 +1,32 @@
+//Exercise 1 : Variable Declaration
+
+let name = 'karabo';
+let age = 26;
+isStudent = false;
+
+console.log(name);
+console.log(age);
+console.log(isStudent);
+
+//Exercise 2 :Variable Reassignment
+
+let favoritColor = 'blue';
+console.log(favoritColor);
+favoritColor ='green';
+console.log(favoritColor);
+
+// Exercise 3: Arethmetic Operators
+
+let num1 = 32;
+let num2 = 40;
+
+let sum = num1 + num2;
+let product =num1 * num2;
+let diffence = num1 - num2;
+let quotient = num1 % num1;
+
+console.log("Sum = "+sum);
+console.log("Product = "+product);
+console.log("diffence = "+diffence);
+console.log("Quotient = "+quotient);
+
