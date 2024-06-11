84ede8a486f46217ff24e0f4cb4a4b998eb4b0bf
Mashapa <mashapakarabo9@gmail.com>
2024-06-11 16:05:16 +0200

    unfished conditional_statements

---
+++ conditional_statements.js
@@ -0,0 +1,29 @@
+//Exercise 1: Tempareture check
+
+let temparature;
+
+if(temparature < 0 )
+    {
+        console.log("its freezing");
+    }
+
+else if(temparature >=0 && temparature <=15)
+    {
+        console.log("Its cold");
+    }
+else if(temparature >=16 && temparature <= 25 )
+    {
+        console.log("its mild");
+    }
+    else if(temparature>25)
+        {
+            console.log("its warm");
+        }
+
+    //Exercise 2: switch case
+
+    switch(temparature)
+    {
+        case  :
+
+    }
